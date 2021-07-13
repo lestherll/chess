ec63b8453cb317e83282ecf0bd9e7ef2eb3524a6
lestherll <qweqwe123.aa690@gmail.com>
2021-07-13 16:32:49 +0100

    add generate_move for pawn, rook, bishop, queen and king

--- chess/model/pieces.py
+++ chess/model/pieces.py
@@ -2,14 +2,15 @@ from ..types import Colour
 
 
 class Piece:
-    def __init__(self, colour: Colour) -> None:
+    def __init__(self, colour: Colour, has_moved: bool = False) -> None:
         self.colour: Colour = colour
+        self.has_moved = has_moved
 
     def __repr__(self) -> str:
         return f"Piece(colour={self.colour})"
 
     def __str__(self) -> str:
-        return f"P{self.colour[0]}"
+        return f"{self.__class__.__name__[:2]}{self.colour[0]}"
 
 
 class Pawn(Piece):
