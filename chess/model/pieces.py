87771a0e68ebef9d5073a4ad95d0519f666618f2
lestherll <ljllacuna4@gmail.com>
2021-07-13 14:04:11 +0100

    Add str of Piece for debugging

--- chess/model/pieces.py
+++ chess/model/pieces.py
@@ -3,10 +3,13 @@ from ..types import Colour
 
 class Piece:
     def __init__(self, colour: Colour) -> None:
-        self.colour = colour
+        self.colour: Colour = colour
 
     def __repr__(self) -> str:
         return f"Piece(colour={self.colour})"
+
+    def __str__(self) -> str:
+        return f"P{self.colour[0]}"
 
 
 class Pawn(Piece):
