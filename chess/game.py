727b7303f4fba51d30a1252fbc1bfab933c74b21
lestherll <qweqwe123.aa690@gmail.com>
2021-07-13 17:45:50 +0100

    reduced hardcoding for Game._setup

--- chess/game.py
+++ chess/game.py
@@ -14,23 +14,35 @@ class Game:
             self.board[1][i].piece = Pawn("BLACK")
             self.board[6][i].piece = Pawn("WHITE")
 
-        self.board[0][0].piece = Rook("BLACK")
-        self.board[0][7].piece = Rook("BLACK")
-        self.board[7][0].piece = Rook("WHITE")
-        self.board[7][7].piece = Rook("WHITE")
-
-        self.board[0][1].piece = Knight("BLACK")
-        self.board[0][6].piece = Knight("BLACK")
-        self.board[7][1].piece = Knight("WHITE")
-        self.board[7][6].piece = Knight("WHITE")
-
-        self.board[0][2].piece = Bishop("BLACK")
-        self.board[0][5].piece = Bishop("BLACK")
-        self.board[7][2].piece = Bishop("BLACK")
-        self.board[7][5].piece = Bishop("BLACK")
+        for i, piece in zip(range(3), [Rook, Knight, Bishop]):
+            self.board[0][i].piece = piece("BLACK")
+            self.board[0][7 - i].piece = piece("BLACK")
+            self.board[7][i].piece = piece("WHITE")
+            self.board[7][7 - i].piece = piece("WHITE")
 
         self.board[0][3].piece = Queen("BLACK")
         self.board[7][3].piece = Queen("WHITE")
 
         self.board[0][4].piece = King("BLACK")
         self.board[7][4].piece = King("WHITE")
+
+        # self.board[0][0].piece = Rook("BLACK")
+        # self.board[0][7].piece = Rook("BLACK")
+        # self.board[7][0].piece = Rook("WHITE")
+        # self.board[7][7].piece = Rook("WHITE")
+        #
+        # self.board[0][1].piece = Knight("BLACK")
+        # self.board[0][6].piece = Knight("BLACK")
+        # self.board[7][1].piece = Knight("WHITE")
+        # self.board[7][6].piece = Knight("WHITE")
+        #
+        # self.board[0][2].piece = Bishop("BLACK")
+        # self.board[0][5].piece = Bishop("BLACK")
+        # self.board[7][2].piece = Bishop("BLACK")
+        # self.board[7][5].piece = Bishop("BLACK")
+
+        # self.board[0][3].piece = Queen("BLACK")
+        # self.board[7][3].piece = Queen("WHITE")
+        #
+        # self.board[0][4].piece = King("BLACK")
+        # self.board[7][4].piece = King("WHITE")
