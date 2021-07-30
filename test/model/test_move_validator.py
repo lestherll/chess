a0eb872eb82628fcf260c97356bfdc820f4f6c8e
lestherll <ljllacuna4@gmail.com>
2021-07-30 22:59:04 +0100

    added bishop movement test cases

--- test/model/test_move_validator.py
+++ test/model/test_move_validator.py
@@ -4,13 +4,12 @@ from chess.custom_typehints import MoveSet
 from chess.model.game import Game
 from chess.model.board import Board
 from chess.model.move_validator import generate_move
-from chess.model.pieces import Pawn, Rook
+from chess.model.pieces import Pawn, Rook, Bishop
 
 
 class TestMoveValidator(TestCase):
 
     def test_generate_move(self):
-
         # generate initial game with board object
         game: Game = Game()
         board: Board = game.board
@@ -44,23 +43,23 @@ class TestMoveValidator(TestCase):
         board.clear()
         board[1][1].piece = Pawn("BLACK")
         board[3][1].piece = Pawn("WHITE")
-        self.assertSetEqual({(1, 2)}, generate_move(board=board, coord=(1, 1)), msg=board)
+        self.assertSetEqual({(1, 2)}, generate_move(board=board, coord=(1, 1)))
 
         board.clear()
         board[6][0].piece = Pawn("WHITE")
         board[5][0].piece = Pawn("BLACK")
-        self.assertSetEqual(set(), generate_move(board=board, coord=(0, 6)), msg=board)
+        self.assertSetEqual(set(), generate_move(board=board, coord=(0, 6)))
 
         board.clear()
         board[6][1].piece = Pawn("WHITE")
         board[4][1].piece = Pawn("BLACK")
-        self.assertSetEqual({(1, 5)}, generate_move(board=board, coord=(1, 4)), msg=board)
+        self.assertSetEqual({(1, 5)}, generate_move(board=board, coord=(1, 4)))
 
         board.clear()
         board[6][0].piece = Pawn("BLACK")
-        self.assertSetEqual({(0, 7)}, generate_move(board=board, coord=(0, 6)), msg=board)
+        self.assertSetEqual({(0, 7)}, generate_move(board=board, coord=(0, 6)))
         board[1][0].piece = Pawn("WHITE")
-        self.assertSetEqual({(0, 0)}, generate_move(board=board, coord=(0, 1)), msg=board)
+        self.assertSetEqual({(0, 0)}, generate_move(board=board, coord=(0, 1)))
 
     def test_pawn_take_move(self):
         board: Board = Board()
@@ -81,14 +80,14 @@ class TestMoveValidator(TestCase):
         # 2    |    | PW
         board[1][1].piece = Pawn("BLACK")
         board[2][2].piece = Pawn("WHITE")
-        self.assertIn((2, 2), generate_move(board=board, coord=(1, 1)), msg=board)
+        self.assertIn((2, 2), generate_move(board=board, coord=(1, 1)))
 
         # added white pawn to lower left side of black pawn at (1, 1)
         # 1    | PB |
         #  ----|----|----
         # 2 PW |    | PW
         board[2][0].piece = Pawn("WHITE")
-        self.assertIn((0, 2), generate_move(board=board, coord=(1, 1)), msg=board)
+        self.assertIn((0, 2), generate_move(board=board, coord=(1, 1)))
 
         # changed colour of pawn at (x=0, y=2) from white to black hence
         # black pawn at (x=1, y=1) cannot take it
@@ -109,7 +108,7 @@ class TestMoveValidator(TestCase):
             (0, 1), (0, 2), (0, 3)
         }
         actual_moveset: MoveSet = generate_move(board=board, coord=(0, 0))
-        self.assertSetEqual(expected_moveset, actual_moveset, msg=actual_moveset)
+        self.assertSetEqual(expected_moveset, actual_moveset)
 
     def test_rook_blocked_move_by_ally(self):
         # prepare 4x4 empty board with 1 white rook at (0, 0)
@@ -122,19 +121,19 @@ class TestMoveValidator(TestCase):
             (0, 1), (0, 2), (0, 3)
         }
         actual_moveset: MoveSet = generate_move(board=board, coord=(0, 0))
-        self.assertSetEqual(expected_moveset, actual_moveset, msg=actual_moveset)
+        self.assertSetEqual(expected_moveset, actual_moveset)
 
         board.put_piece((0, 1), Pawn("WHITE"))
         expected_moveset = set()
         actual_moveset = generate_move(board=board, coord=(0, 0))
-        self.assertSetEqual(expected_moveset, actual_moveset, msg=actual_moveset)
+        self.assertSetEqual(expected_moveset, actual_moveset)
 
-        board.remove_piece_at(from_block=(1, 0))
+        board.remove_piece_at(from_coord=(1, 0))
         expected_moveset = {
             (1, 0), (2, 0), (3, 0)
         }
         actual_moveset: MoveSet = generate_move(board=board, coord=(0, 0))
-        self.assertSetEqual(expected_moveset, actual_moveset, msg=actual_moveset)
+        self.assertSetEqual(expected_moveset, actual_moveset)
 
     def test_rook_blocked_move_by_enemy(self):
         board: Board = Board(_length=4)
@@ -155,4 +154,45 @@ class TestMoveValidator(TestCase):
             (1, 0)
         }
         actual_moveset: MoveSet = generate_move(board=board, coord=(0, 0))
+        self.assertSetEqual(expected_moveset, actual_moveset)
+
+    def test_bishop_basic_move(self):
+        board: Board = Board(_length=4)
+        board.put_piece((2, 2), Bishop("WHITE"))
+
+        expected_moveset: MoveSet = {
+            (1, 1), (0, 0),
+            (1, 3),
+            (3, 1),
+            (3, 3)
+        }
+        actual_moveset: MoveSet = generate_move(board=board, coord=(2, 2))
+        self.assertSetEqual(expected_moveset, actual_moveset)
+
+    def test_bishop_blocked_move_by_ally(self):
+        board: Board = Board(_length=4)
+        board.put_piece((2, 2), Bishop("WHITE"))
+        board.put_piece((1, 1), Pawn("WHITE"))
+        board.put_piece((3, 3), Pawn("WHITE"))
+
+        expected_moveset: MoveSet = {
+            (1, 3),
+            (3, 1)
+        }
+        actual_moveset: MoveSet = generate_move(board=board, coord=(2, 2))
+        self.assertSetEqual(expected_moveset, actual_moveset)
+
+    def test_bishop_blocked_move_by_enemy(self):
+        board: Board = Board(_length=4)
+        board.put_piece((2, 2), Bishop("WHITE"))
+        board.put_piece((1, 1), Pawn("BLACK"))
+        board.put_piece((3, 3), Pawn("BLACK"))
+
+        expected_moveset: MoveSet = {
+            (1, 1),
+            (1, 3),
+            (3, 1),
+            (3, 3)
+        }
+        actual_moveset: MoveSet = generate_move(board=board, coord=(2, 2))
         self.assertSetEqual(expected_moveset, actual_moveset)
